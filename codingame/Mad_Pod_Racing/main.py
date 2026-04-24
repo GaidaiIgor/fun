918c6e2907062279aa18707e9186f0b9196de0b9
Igor Gaidai <gaidai92@gmail.com>
2026-04-24 01:12:41 -0400

    Tracks velocity

--- codingame/Mad_Pod_Racing/main.py
+++ codingame/Mad_Pod_Racing/main.py
@@ -1,17 +1,25 @@
 """Runs a simple Mad Pod Racing bot."""
 
+import sys
 from dataclasses import dataclass
 
 import numpy as np
 from numpy.typing import NDArray
 
 
+DRAG_FACTOR = 0.15
+BOOST_THRUST = 650
+MAX_TURN_DEG = 18
+
+
 @dataclass(slots=True)
 class Player:
     """Stores one pod state.
     :var position: Pod center coordinates.
+    :var velocity: Pod velocity vector since the previous turn.
     """
     position: NDArray[int]
+    velocity: NDArray[int]
 
 
 @dataclass(slots=True)
@@ -36,7 +44,17 @@ def main():
     """Runs the game loop."""
     game_state = None
     while True:
-        game_state = read_game_state(game_state)
+        prev_game_state = game_state
+        game_state = read_game_state(prev_game_state)
+
+        abs_velocity_change = np.linalg.norm(game_state.player.velocity - prev_game_state.player.velocity) if prev_game_state else 0
+        print(f"pos={game_state.player.position} vel={game_state.player.velocity} d|v|={abs_velocity_change:g}", file=sys.stderr)
+
+        # if np.linalg.norm(game_state.player.velocity) < 300:
+        #     print(game_state.player.position[0], 0, 100)
+        # else:
+        #     print(game_state.player.position[0], 0, 0)
+
         thrust = 0 if abs(game_state.next_check_angle) > 90 else 100
         if game_state.next_check_angle == 0 and game_state.next_check_dist > 5000 and game_state.boosts:
             thrust = "BOOST"
@@ -44,16 +62,20 @@ def main():
         print(*game_state.next_check_pos, thrust)
 
 
-def read_game_state(previous_game_state: GameState | None) -> GameState:
+def read_game_state(prev_game_state: GameState | None) -> GameState:
     """Reads the current turn input.
-    :param previous_game_state: Previous turn state carrying persistent data.
+    :param prev_game_state: Previous turn state carrying persistent data.
     :return: Parsed game state for the current turn.
     """
     x, y, next_check_x, next_check_y, next_check_dist, next_check_angle = map(int, input().split())
     opponent_x, opponent_y = map(int, input().split())
-    boosts = 1 if previous_game_state is None else previous_game_state.boosts
-    return GameState(player=Player(np.array((x, y))), opponent=Player(np.array((opponent_x, opponent_y))),
-                     next_check_pos=np.array((next_check_x, next_check_y)), next_check_dist=next_check_dist, next_check_angle=next_check_angle, boosts=boosts)
+    player_pos = np.array((x, y))
+    opponent_pos = np.array((opponent_x, opponent_y))
+    player_velocity = np.zeros(2, dtype=int) if prev_game_state is None else player_pos - prev_game_state.player.position
+    opponent_velocity = np.zeros(2, dtype=int) if prev_game_state is None else opponent_pos - prev_game_state.opponent.position
+    boosts = 1 if prev_game_state is None else prev_game_state.boosts
+    return GameState(Player(player_pos, player_velocity), Player(opponent_pos, opponent_velocity), np.array((next_check_x, next_check_y)),
+                     next_check_dist, next_check_angle, boosts)
 
 
 main()

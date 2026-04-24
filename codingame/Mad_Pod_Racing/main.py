b43de51632d21d80f833b6dd0c0fee740a077c2d
Igor Gaidai <gaidai92@gmail.com>
2026-04-23 23:23:10 -0400

    Mad Pod Race initial commit

---
+++ codingame/Mad_Pod_Racing/main.py
@@ -0,0 +1,20 @@
+BOOSTS = 1
+
+
+def main():
+    while True:
+        x, y, next_checkpoint_x, next_checkpoint_y, next_checkpoint_dist, next_checkpoint_angle = [int(i) for i in input().split()]
+        opponent_x, opponent_y = [int(i) for i in input().split()]
+
+        if next_checkpoint_angle > 90 or next_checkpoint_angle < -90:
+            thrust = 0
+        else:
+            thrust = 100
+        if next_checkpoint_angle == 0 and next_checkpoint_dist > 5000 and BOOSTS > 0:
+            thrust = 'BOOST'
+            BOOSTS -= 1
+
+        print(f'{next_checkpoint_x} {next_checkpoint_y} {thrust}')
+
+
+main()

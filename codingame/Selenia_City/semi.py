279829e885cc53f1ff5bf061aa194ab4676bbee4
Igor Gaidai <gaidai92@gmail.com>
2026-07-04 15:52:57 -0400

    Stop padding dynamic paths after demand ends

--- codingame/Selenia_City/semi.py
+++ codingame/Selenia_City/semi.py
@@ -1093,18 +1093,14 @@ def service_area_connected(area: set[Pair]) -> bool:
 
 
 def normalize_month_path(path: list[int]) -> list[int]:
-    """Extends or trims path to the game month path length."""
+    """Trims path and prevents accidental looping after demand is exhausted."""
     if len(path) >= MONTH_DAYS + 1:
         return path[:MONTH_DAYS + 1]
     if len(path) < 2:
         return path[:]
-    edges = list(zip(path, path[1:]))
-    if path[0] != path[-1]:
-        edges.extend(zip(path[-1:0:-1], path[-2::-1]))
-    result = [path[0]]
-    for day in range(MONTH_DAYS):
-        result.append(edges[day % len(edges)][1])
-    return result
+    if path[0] == path[-1]:
+        return path + [path[-2]]
+    return path
 
 
 def fixed_next_index(path: list[int], index: int) -> int:

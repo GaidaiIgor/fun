394148898bde3e202b3a1c009b0f32e8095eefdf
Igor Gaidai <gaidai92@gmail.com>
2026-08-09 01:24:49 -0400

    Apply capped-source uniformity rule

--- codingame/Selenia_City/rules.md
+++ codingame/Selenia_City/rules.md
@@ -68,8 +68,8 @@ Pods fulfill assigned loads.
 4. If initial assignments are such that capacity of a given load is exceeded or assignments are non-uniform, fix this by considering lower priority assignments
 for some of the pods violating these rules in the order of their preference list.
    1. Capacity is defined as the available number of batches (ceil(passengers / 10))
-   2. Uniform assignment is defined as a distribution of pods over loads such that for any non-capped load the number of assigned pods does not exceed the
-   number of pods assigned to any other non-capped load by more than 1.
+   2. Uniform assignment is defined as a distribution of pods over loads such that for any load the number of assigned pods does not exceed the number of pods
+   assigned to any other non-capped load by more than 1.
       1. Capped load is defined as the load with the number of assigned pods equal to load capacity.
       2. Uniformity is evaluated separately for each priority level.
    3. When deciding which pods should remain assigned:
--- codingame/Selenia_City/semi.py
+++ codingame/Selenia_City/semi.py
@@ -14,7 +14,7 @@ REROUTE_COST = POD_COST - POD_REFUND
 TELEPORT_COST = 5000
 INF = 10 ** 9
 OVERRIDE_MONTH = 10
-OVERRIDE_COMMAND = "TUBE 2 7;TUBE 4 8;POD 3 AUTO;POD 4 AUTO;POD 5 AUTO;POD 6 AUTO"
+OVERRIDE_COMMAND = "TUBE 4 8;TUBE 2 7;UPGRADE 2 3;POD 3 AUTO;POD 4 AUTO;POD 2 AUTO"
 FULL_DEBUG = False
 _G = {}
 BY_ID = attrgetter("id")
@@ -1188,8 +1188,8 @@ class Planner:
                     if indices[pod_id] + 1 < len(preferences[pod_id]) or may_unassign]
                 if not pods:
                     continue
-                uneven = counts[path] != capacities[path] and any(candidate.priority == path.priority and
-                    counts[candidate] != capacities[candidate] and counts[candidate] < counts[path] - 1 for candidate in paths)
+                uneven = any(candidate.priority == path.priority and counts[candidate] != capacities[candidate] and
+                    counts[candidate] < counts[path] - 1 for candidate in paths)
                 if counts[path] <= capacities[path] and not uneven:
                     continue
                 pod_id = max(pods, key=lambda item: (distances[item, path.nodes[0]], item))
